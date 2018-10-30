6ad6ef4c9b92465f8bd188d4e95c95a29e722884
Simon Parmenter <fractal356pragma@virginmedia.com>
2018-10-30 21:22:47 +0000

    Remove '=' for array initialisation

--- glfwcpp/interface/glfwpp.hpp
+++ glfwcpp/interface/glfwpp.hpp
@@ -10,16 +10,16 @@ namespace GLFWPP_ns
 constexpr auto const HINT_DONT_CARE = -1;
 
 enum class GL_API : char {OPENGL, ES, N};
-char const * const GL_API_STR[static_cast<char>(GL_API::N)] = {"OPENGL", "ES"};
+char const * const GL_API_STR[static_cast<char>(GL_API::N)]{"OPENGL", "ES"};
 
 enum class GL_PROFILE : char {ANY, COMPAT, CORE, N};
-char const * const GL_PROFILE_STR[static_cast<char>(GL_PROFILE::N)] = {"ANY", "COMPAT", "CORE"};
+char const * const GL_PROFILE_STR[static_cast<char>(GL_PROFILE::N)]{"ANY", "COMPAT", "CORE"};
 
 enum class GL_ROBUSTNESS : char {NONE, NO_RESET_NOTIFICATION, LOSE_CONTEXT_ON_RESET, N};
-char const * const GL_ROBUSTNESS_STR[static_cast<char>(GL_ROBUSTNESS::N)] = {"NONE", "NO_RESET_NOTIFICATION", "LOSE_CONTEXT_ON_RESET"};
+char const * const GL_ROBUSTNESS_STR[static_cast<char>(GL_ROBUSTNESS::N)]{"NONE", "NO_RESET_NOTIFICATION", "LOSE_CONTEXT_ON_RESET"};
 
 enum class GL_CONTEXT_RELEASE_BEHAVIOUR : char {ANY, FLUSH, NONE, N};
-char const * const GL_CONTEXT_RELEASE_BEHAVIOUR_STR[static_cast<char>(GL_CONTEXT_RELEASE_BEHAVIOUR::N)] = {"ANY", "FLUSH", "NONE"};
+char const * const GL_CONTEXT_RELEASE_BEHAVIOUR_STR[static_cast<char>(GL_CONTEXT_RELEASE_BEHAVIOUR::N)]{"ANY", "FLUSH", "NONE"};
 
 struct versionAttributes
 {
