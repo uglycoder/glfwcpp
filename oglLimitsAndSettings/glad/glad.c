81ef752604ae9588ccaa1a8371335a6b07697d13
Simon Parmenter <noreply@noreply.com>
2018-11-18 22:51:57 +0000

    Changed the interface function, output a struct containing a map of std pairs of limits and a descriptive string of the limit Restructured directories, files.

--- oglLimitsAndSettings/glad/glad.c
+++
@@ -1,2 +0,0 @@
-#include "glad.h"
-#include <glad/glad.c>
--- oglLimitsAndSettings/interface/glheader.h
+++
@@ -1,8 +0,0 @@
-// glheader.h
-//  MIT License
-//  See LICENSE.txt file in root of project
-//  Copyright(c) 2018 Simon Parmenter
-
-#pragma once
-
-#include "../glad/glad.h"
