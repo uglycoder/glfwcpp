6abe7e8289a810babfc5b2aed8fa9e8083651e92
Simon Parmenter <noreply@noreply.com>
2018-12-17 22:35:11 +0000

    Create files for texture samplers

---
+++ glfwcpp/interface/texturesampler.hpp
@@ -0,0 +1,17 @@
+//  texturesampler.hpp
+//  MIT License
+//  See LICENSE.txt file in root of project
+//  Copyright(c) 2018 Simon Parmenter
+
+#pragma once
+
+namespace GLFWPP_ns
+{
+  class TextureSampler
+  {
+  public:
+    TextureSampler();
+    ~TextureSampler();
+  };
+
+}
---
+++ glfwcpp/src/texturesampler.cpp
@@ -0,0 +1,17 @@
+//  texturesampler.cpp
+//  MIT License
+//  See LICENSE.txt file in root of project
+//  Copyright(c) 2018 Simon Parmenter
+
+#include "precompiled-header.hpp"
+#include "../interface/texturesampler.hpp"
+
+
+GLFWPP_ns::TextureSampler::TextureSampler()
+{
+}
+
+
+GLFWPP_ns::TextureSampler::~TextureSampler()
+{
+}
